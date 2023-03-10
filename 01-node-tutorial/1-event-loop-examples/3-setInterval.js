29f7ab391203d0ba7781928a6f54561f800b34e3
Steph <32989597+stephskas@users.noreply.github.com>
2023-03-10 15:21:41 -0800

    add answers directory with lessons 01-12

--- 01-node-tutorial/1-event-loop-examples/3-setInterval.js
+++ 01-node-tutorial/1-event-loop-examples/3-setInterval.js
@@ -1,7 +1,7 @@
 setInterval(() => {
-  console.log('hello world')
-}, 2000)
-console.log(`I will run first`)
+	console.log('hello world');
+}, 2000);
+console.log(`I will run first`);
 // process stays alive unless
 // Kill Process CONTROL + C
 // unexpected error
