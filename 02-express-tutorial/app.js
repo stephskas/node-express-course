8da3b2949da7220d4bfd4de0eb7a8beb91e8e749
Steph <32989597+stephskas@users.noreply.github.com>
2023-03-13 09:24:48 -0700

    moved answers into 02 directory

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,64 +1,19 @@
 const express = require('express');
 const app = express();
-const { products } = require('./data');
+// req => middleware => res
 
 app.get('/', (req, res) => {
-	res.send('<h1>Home Page</h1><a href="/api/products">products</a>');
+	const method = req.method;
+	const url = req.url;
+	const time = new Date().getFullYear();
+	console.log(method, url, time);
+	res.send('Home');
 });
 
-app.get('/api/products', (req, res) => {
-	const newProducts = products.map((product) => {
-		const { id, name, image } = product;
-		return { id, name, image };
-	});
-	res.json(newProducts);
+app.get('/about', (req, res) => {
+	res.send('About');
 });
 
-app.get('/api/products/:productID', (req, res) => {
-	// console.log(req);
-	// console.log(req.params);
-	const { productID } = req.params;
-	const singleProduct = products.find(
-		(product) => product.id === Number(productID)
-	);
-	if (!singleProduct) {
-		return res.status(404).send('Product Does Not Exist');
-	}
-	res.json(singleProduct);
-});
-
-app.get('/api/products/:productID/reviews/:reviewID', (req, res) => {
-	console.log(req.params);
-	res.send('hello world');
-});
-
-app.get('/api/v1/query', (req, res) => {
-	console.log(req.query);
-	const { search, limit } = req.query;
-	let sortedProducts = [...products];
-
-	if (search) {
-		sortedProducts = sortedProducts.filter((product) => {
-			return product.name.startsWith(search);
-		});
-	}
-	if (limit) {
-		sortedProducts = sortedProducts.slice(0, Number(limit));
-	}
-	if (sortedProducts.length < 1) {
-		// res.status(200).send('no products matched your search');
-		return res.status(200).json({ success: true, data: [] }); // only one response per request, need to have a return
-	}
-	return res.status(200).json(sortedProducts);
-
-	// test with http://localhost:5002/api/v1/query?name=john&id=4
-	// test with http://localhost:5002/api/v1/query?search=a&limit=2
-});
-
-app.get('*', (req, res) => {
-	res.status(404).send('resource not found');
-});
-
-app.listen(5002, () => {
-	console.log('server is listening on port: 5002...');
+app.listen(5001, () => {
+	console.log('Server is listening on port 5001...');
 });
