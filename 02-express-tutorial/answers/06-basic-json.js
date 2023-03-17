0e94fe2dc3827846e088dfea8a6b744c0376ccc1
Steph Sakai <steph@Stephs-MBP.attlocal.net>
2023-03-17 11:08:22 -0700

    replacing 06 file

---
+++ 02-express-tutorial/answers/06-basic-json.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const { products } = require('./data');
+
+app.get('/', (req, res) => {
+	res.status(200).json(products);
+});
+
+app.get('*', (req, res) => {
+	res.status(404).send('resource not found');
+});
+
+app.listen(5002, () => {
+	console.log('server is listening on port: 5002...');
+});
