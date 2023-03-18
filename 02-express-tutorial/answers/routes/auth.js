0ed85e7c1ed58ff61e6e88609c8832b7507518d2
Steph Sakai <steph@Stephs-MBP.attlocal.net>
2023-03-18 13:29:03 -0700

    modifying 13-router-people.js and auth.js

--- 02-express-tutorial/answers/routes/auth.js
+++ 02-express-tutorial/answers/routes/auth.js
@@ -3,10 +3,9 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
 	const { name } = req.body;
-	if (name) {
-		res.status(200).send(`Welcome ${name}`);
-	}
-	res.status(401).send('Please enter statement');
+	name
+		? res.status(200).send(`Welcome ${name}`)
+		: res.status(401).send('Please enter statement');
 });
 
 module.exports = router;
--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,1 +1,17 @@
-console.log('Express Tutorial')
+const express = require('express');
+const app = express();
+const router = express.Router();
+const people = require('./answers/routes/people');
+const auth = require('./answers/routes/auth');
+
+// static assets
+app.use(express.static('./methods-public'));
+// parse form data
+app.use(express.urlencoded({ extended: false }));
+// parse json
+app.use(express.json());
+app.use('/api/people', people);
+app.use('/login', auth);
+app.listen(5001, () => {
+	console.log('listening on port 5001');
+});
